a572f15aa602f899c4a2ea3189b767117d6ef5d8
Boris Callens <boris_callens@hotmail.com>
2012-09-27 16:41:07 +0200

    Remove now works correctly. Re-enabled unit tests

--- Crafty.HashMap/new.js
+++ Crafty.HashMap/new.js
@@ -20,9 +20,6 @@ NewHashMap.prototype.key = function (obj) {
 		y2: Math.floor((obj._h + obj._y) / this.cellsize)
 	};
 };
-NewHashMap.prototype.hash = function (keys) {
-	
-};
 NewHashMap.prototype.insert = function (obj) {
 	var keys = this.key(obj);
 	var entry = new Entry(keys, obj, this);
@@ -41,22 +38,6 @@ NewHashMap.prototype.insert = function (obj) {
 		if(!this.rows[i]){this.rows[i] = []};
 		this.rows[i].push(entry);
 	}
-	
-	// //TODO: fase all this shit out
-	// var col, row;	
-	// for (col = keys.x1; col <= keys.x2; col++) {
-		// var currentCol = this.map[col];
-		// if (!currentCol) this.map[col] = currentCol = { minRow: keys.y1, maxRow: keys.y2 };
-		
-		// currentCol.minRow = Math.min(currentCol.minRow, keys.y1);
-		// currentCol.maxRow = Math.max(currentCol.maxRow, keys.y2);
-		
-		// for (row = keys.y1; row <= keys.y2; row++) {
-			// var currentRow =currentCol[row];
-			// if (!currentRow) currentCol[row] = [];
-			// this.map[col][row].push(entry);
-		// }
-	// }	
 };
 
 NewHashMap.prototype.isContainedBy = function(entry, keys){
@@ -76,9 +57,28 @@ NewHashMap.prototype.remove = function (keys, obj) {
 	}
 	
 	var me = this;
+	
+	// remove the obj from the entries
 	me.entries = me.entries.filter(function(entry){
 		return me.isContainedBy(entry, keys) && entry.obj !== obj;
 	});
+	// remove it from column and row
+	for(var i = keys.x1; i <= keys.x2; i++){
+		if(this.columns[i]){
+			this.columns[i] = this.columns[i].filter(function(entry){
+				return me.isContainedBy(entry, keys) && entry.obj !== obj;
+			});
+		};
+	}
+	for(var i = keys.y1; i <= keys.y2; i++){
+		if(this.rows[i]){
+			this.rows[i] = this.rows[i].filter(function(entry){
+				return me.isContainedBy(entry, keys) && entry.obj !== obj;
+			});
+		};
+	}
+	
+	// recalculate minX, maxX, minY and maxY	
 	var allX = [];
 	var allY = [];
 	for (var i = 0; i <= me.entries.length-1; i++){
@@ -90,45 +90,7 @@ NewHashMap.prototype.remove = function (keys, obj) {
 	this.minX = Math.min.apply(Math, allX);
 	this.maxX = Math.max.apply(Math, allX);
 	this.minY = Math.min.apply(Math, allY);
-	this.maxY = Math.max.apply(Math, allY);
-	
-	//TODO: remove from cols and rows;
-	
-	//TODO: fase all this shit out
-	// var currentCol, currentRow, len, col, row, cell;
-	// var myMap = this.map
-	
-	// for (col = keys.x1; col <= keys.x2; col++) {
-		// currentCol = myMap[col];
-		// if (!currentCol) continue;
-
-		// for (row = keys.y1; row <= keys.y2; row++) {
-			// currentRow = currentCol[row];
-			// if (!currentRow) continue;
-
-			// len = currentRow.length;
-			// for (cell = len-1; cell >= 0; cell--) {
-				// if (currentRow[cell] && currentRow[cell][0] === obj[0]) {
-					// currentRow.splice(cell, 1);
-				// }
-			// }			
-		// }
-		// //recalculate minrow and maxrow
-		// var keys = Object.keys(currentCol).map(Number).filter(function(a){
-			// return isFinite(a) && currentCol[a] && currentCol[a].len > 0;
-		// });
-		// currentCol.minRow = Math.min.apply(Math, keys);
-		// currentCol.maxRow = Math.max.apply(Math, keys);
-	// }
-	// var keys = Object.keys(myMap).map(Number);
-	// keys = keys.filter(function(a){
-			// return isFinite(a) && myMap[a] && isFinite(myMap[a].minRow);
-	// });
-	
-	// //todo: update minx, maxx etc
-	
-	// this.minCol = Math.min.apply(Math, keys);
-	// this.maxCol = Math.max.apply(Math, keys);
+	this.maxY = Math.max.apply(Math, allY);	
 };
 NewHashMap.prototype.search = function (rect, filter) {
 	//Optional parameter handling
@@ -141,7 +103,7 @@ NewHashMap.prototype.search = function (rect, filter) {
 	var rows = this.rows.slice(keys.x1, keys.x2);
 	if(rows.length == 0){return;};
 	
-	// Get the elements in the correct collumns
+	// Get the elements in the correct columns
 	var cols = this.columns.slice(keys.y1, keys.y2);
 	if(cols.length == 0){return;}
 	
@@ -150,62 +112,22 @@ NewHashMap.prototype.search = function (rect, filter) {
 	var rowElements = [].concat.apply([], rows).filter(function(a){return a});
 	
 	var intersection =  getIntersect(colElements, rowElements);
+	return intersection;	
+	// Not sure where to put this..
 	function getIntersect(arr1, arr2) {
-    var r = [], o = {}, l = arr2.length, i, v;
-    for (i = 0; i < l; i++) {
-        o[arr2[i]] = true;
-    }
-    l = arr1.length;
-    for (i = 0; i < l; i++) {
-        v = arr1[i];
-        if (v in o) {
-            r.push(v);
-        }
-    }
-    return r;
-}
-	return intersection;
-	//return entries.filter(me.isContainedBy(entry, keys));
-	// if (!filter){return found}
-	
-	// //TODO: filter and phase old shit out
-	// var results = [];
-	// var currentCol, currentRow, startRow, endRow, col, row;
-	
-	// var startCol = Math.max(keys.x1, this.minCol);
-	// var endCol = Math.min(keys.x2, this.maxCol);
-
-	// for (col = startCol; col <= endCol; col++) {
-		// currentCol = this.map[col];
-		// if (!currentCol) continue;
-
-		// startRow = Math.max(keys.y1, currentCol.minRow);
-		// endRow = Math.min(keys.y2, currentCol.maxRow);
-		// for (row = startRow; row <= endRow; row++) {
-			// currentRow = currentCol[row];
-			// if (currentRow) {
-				// results = results.concat(currentRow);
-			// }
-		// }
-	// }
-	// if (!filter){return results}
-	
-	// var i, obj, id, finalresult = [], found = {};
-	// var len = results.length;
-	// for (i = 0; i < len; i++) {
-		// obj = results[i];
-		// if (!obj) continue;
-
-		// id = obj[0];
-		// if (!found[id] && obj.x < rect._x + rect._w && obj._x + obj._w > rect._x && obj.y < rect._y + rect._h && obj._h + obj._y > rect._y) {
-			// found[id] = results[i];
-		// }
-	// }
-	// for (obj in found) {
-		// finalresult.push(found[obj]);
-	// }
-	// return finalresult;
-	
+		var r = [], o = {}, l = arr2.length, i, v;
+		for (i = 0; i < l; i++) {
+			o[arr2[i]] = true;
+		}
+		l = arr1.length;
+		for (i = 0; i < l; i++) {
+			v = arr1[i];
+			if (v in o) {
+				r.push(v);
+			}
+		}
+		return r;
+	}
 };
 NewHashMap.prototype.boundaries = function () {
 	return {
--- Crafty.HashMap/qunit-tests.js
+++ Crafty.HashMap/qunit-tests.js
@@ -1,141 +1,107 @@
-/*test("map.insert one entity with x, y, w and h = 1 should insert it at 0,0",1 , function(){
+test("map.insert one entity with x, y, w and h = 1 should insert it in the entries, column and rows",3 , function(){
 	var myMap = new NewHashMap();
 	var entity = {_x:1, _y:1, _w:1, _h:1}
 	myMap.insert(entity);
-	equal(myMap.map[0][0][0].obj, entity);
-});
-test("map.insert one entity with x and y cellsize+1 and h = 1 should insert it at 1,1",1 , function(){
+	
+	equal(myMap.entries[0].obj, entity);
+	equal(myMap.columns[0][0].obj, entity);
+	equal(myMap.rows[0][0].obj, entity);
+});	
+test("map.insert one entity with x and y cellsize+1 and h = 1 should insert it at column 1, row 1",3 , function(){
 	var myMap = new NewHashMap();
 	var size = myMap.cellsize
 	var entity = {_x:size, _y:size, _w:1, _h:1}
 	myMap.insert(entity);
-	equal(myMap.map[1][1][0].obj, entity);
+	
+	equal(myMap.entries[0].obj, entity);
+	equal(myMap.columns[1][0].obj, entity);
+	equal(myMap.rows[1][0].obj, entity);
 });
-test("map.insert 10 entities on position < cellsize should insert all of them at position 0", 10, function(){
+test("map.insert 10 entities on position < cellsize should insert all of them at column 0, row 0", 30, function(){
 	var myMap = new NewHashMap();
 	for (var index = 0; index < 10; index++){
 		var entity = {_x:index, _y:index, _w:1, _h:1}
 		myMap.insert(entity);
-		equal(myMap.map[0][0][index].obj, entity);
+		
+		equal(myMap.entries[index].obj, entity);
+		equal(myMap.columns[0][index].obj, entity);
+		equal(myMap.rows[0][index].obj, entity);
 	}
 });
-test("map.insert 1 entity of 3 tiles wide should insert same object in 3 cells", 3, function(){
+test("map.insert 1 entity of 3 tiles wide should insert same object in 3 cells", 7, function(){
 	var myMap = new NewHashMap();
 	var size = myMap.cellsize
-	var entity = {_x:1, _y:1, _w:size*3, _h:1}
+	var entity = {_x:0, _y:0, _w:size*3-1, _h:1}
 	myMap.insert(entity);
-	equal(myMap.map[0][0][0].obj, entity);
-	equal(myMap.map[1][0][0].obj, entity);
-	equal(myMap.map[2][0][0].obj, entity);
+	
+	equal(myMap.entries.length, 1);
+	equal(myMap.columns.length, 3);
+	equal(myMap.rows.length, 1);
+	
+	equal(myMap.columns[0][0].obj, entity);
+	equal(myMap.columns[1][0].obj, entity);
+	equal(myMap.columns[2][0].obj, entity);
+	equal(myMap.rows[0][0].obj, entity);
 });
-test("map.insert should correctly update mincol", 3, function(){
+test("map.insert should correctly update minx, max, miny, maxy", 16, function(){
 	var myMap = new NewHashMap();
 	var size = myMap.cellsize
 	
-	var entity = {_x:size*4, _y:size*4, _w:1, _h:1}
+	var entity = {_x:0, _y:0, _w:0, _h:0}
 	myMap.insert(entity);
-	equal(myMap.minCol, 4);
+	equal(myMap.minX, 0);
+	equal(myMap.minY, 0);
+	equal(myMap.maxX, 0);
+	equal(myMap.maxX, 0);
 	
-	var entity = {_x:size*3, _y:size*3, _w:1, _h:1}
+	var entity = {_x:10, _y:10, _w:0, _h:0}
 	myMap.insert(entity);
-	equal(myMap.minCol, 3);
+	equal(myMap.minX, 0);
+	equal(myMap.minY, 0);
+	equal(myMap.maxX, 10);
+	equal(myMap.maxX, 10);
 	
-	var entity = {_x:size*99, _y:size*99, _w:1, _h:1}
+	var entity = {_x:10, _y:10, _w:1, _h:1}
 	myMap.insert(entity);
-	equal(myMap.minCol, 3);
+	equal(myMap.minX, 0);
+	equal(myMap.minY, 0);
+	equal(myMap.maxX, 11);
+	equal(myMap.maxX, 11);
+	
+	var size = myMap.cellsize
+	var entity = {_x:0, _y:0, _w:110, _h:110}
+	myMap.insert(entity);
+	equal(myMap.minX, 0);
+	equal(myMap.minY, 0);
+	equal(myMap.maxX, 110);
+	equal(myMap.maxX, 110);
 });
-test("map.insert should correctly update maxcol", 3, function(){
-	var myMap = new NewHashMap();
-	var size = myMap.cellsize
-	
-	var entity = {_x:size*3, _y:1, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.maxCol, 3);
-	
-	var entity = {_x:size*4, _y:1, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.maxCol, 4);
-	
-	var entity = {_x:size*2, _y:1, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.maxCol, 4);
-});
-test("map.insert should correctly update minRow", 3, function(){
-	var myMap = new NewHashMap();
-	var size = myMap.cellsize
-	
-	var entity = {_x:1, _y:size*4, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.map[0].minRow, 4);
-	
-	var entity = {_x:1, _y:size*3, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.map[0].minRow, 3);
-	
-	var entity = {_x:1, _y:size*5, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.map[0].minRow, 3);
-});
-test("map.insert should correctly update maxRow", 3, function(){
-	var myMap = new NewHashMap();
-	var size = myMap.cellsize
-	
-	var entity = {_x:1, _y:size*2, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.map[0].maxRow, 2);
-	
-	var entity = {_x:1, _y:size*3, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.map[0].maxRow, 3);
-	
-	var entity = {_x:1, _y:size*1, _w:1, _h:1}
-	myMap.insert(entity);
-	equal(myMap.map[0].maxRow, 3);
-});*/
-test("map.remove should correctly remove the entry", 2, function(){
+test("map.remove should correctly remove the entry", 3, function(){
 	var myMap = new NewHashMap();
 	var entity = {_x:1, _y:1, _w:1, _h:1}
+	myMap.insert(entity);
+	myMap.remove(entity);
+	
+	equal(myMap.entries.length, 0);
+	equal(myMap.columns[0].length, 0);
+	equal(myMap.rows[0].length, 0);
+});
+test("map.remove should remove the all the matching entries", 6, function(){
+	var myMap = new NewHashMap();
+	var size = myMap.cellsize
+	var entity = {_x:1, _y:1, _w:1, _h:1}
+	myMap.insert(entity);
+	myMap.insert(entity);
 	myMap.insert(entity);
 	equal(myMap.entries[0].obj, entity);
+	equal(myMap.entries[1].obj, entity);
+	equal(myMap.entries[2].obj, entity);
 	
 	myMap.remove(entity);
-	equal(myMap.map[0], undefined);
+	equal(myMap.entries[0], undefined);
+	equal(myMap.entries[1], undefined);
+	equal(myMap.entries[2], undefined);
 });
-/*test("map.remove should remove the entry at all the right depth", 6, function(){
-	var myMap = new NewHashMap();
-	var size = myMap.cellsize
-	var entity = {_x:1, _y:1, _w:1, _h:1}
-	myMap.insert(entity);
-	myMap.insert(entity);
-	myMap.insert(entity);
-	equal(myMap.map[0][0][0].obj, entity);
-	equal(myMap.map[0][0][1].obj, entity);
-	equal(myMap.map[0][0][2].obj, entity);
-	
-	myMap.remove(entity);
-	equal(myMap.map[0][0][0], undefined);
-	equal(myMap.map[0][0][1], undefined);
-	equal(myMap.map[0][0][2], undefined);
-});
-test("map.remove should correctly update mincol", 3, function(){
-	var myMap = new NewHashMap();
-	var size = myMap.cellsize;
-	
-	var entity1 = {_x:size*2, _y:1, _w:1, _h:1}
-	var entity2 = {_x:size*3, _y:1, _w:1, _h:1}
-	var entity3 = {_x:size*4, _y:1, _w:1, _h:1}
-	myMap.insert(entity1);
-	myMap.insert(entity2);
-	myMap.insert(entity3);
-	
-	equal(myMap.minCol, 2);
-	
-	myMap.remove(entity2);
-	equal(myMap.minCol, 2);
-	
-	myMap.remove(entity1);
-	equal(myMap.minCol, 4);
-});*/
 test("map.search without filter should only return result at searched location", function(){
 	var myMap = new NewHashMap();
 	var size = myMap.cellsize;
@@ -156,7 +122,7 @@ test("map.search without filter should only return result at searched location",
 	equal(searchResult[0].obj, entity1);
 	equal(searchResult[1].obj, entity3);
 	equal(searchResult[2].obj, entity2);
-});/*
+});
 // Don't really get what the filter function is for
 // TODO: what?
 // test("map.search with filter should only return objects at location that pass through the filter", function(){
@@ -208,4 +174,4 @@ test("boundaries for two entities should correctly give boundaries", function(){
 	var boundaries = myMap.boundaries();
 		
 	deepEqual(boundaries, expected);
-});*/
+});
